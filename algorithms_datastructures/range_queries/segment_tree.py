fd37769de840c866ae8eaa0aa01a7b25998a4936
darvid7 <bvdarvid@gmail.com>
2017-09-02 03:39:04 +1000

    Add Fenwick (Binary Indexed) tree

---
+++ algorithms_datastructures/range_queries/segment_tree.py
@@ -0,0 +1,10 @@
+"""
+@author: David Lei
+@since: 2/09/2017
+
+Allows queries such as what is the minimum in this range what is the maximum in this range.
+
+http://codeforces.com/blog/entry/6847
+https://www.youtube.com/watch?v=ZBHKZF5w4YU&t=523s&ab_channel=TusharRoy-CodingMadeSimple
+
+"""
